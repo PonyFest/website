122e7cd885d448bf7023cd9353fae6d1c70223f4
Reiuiji <Reiuiji@gmail.com>
2026-01-20 04:07:06 -0500

    Updated website with the new theme

---
+++ static/scripts/svg-override.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const svgObject = document.querySelector('.svgClass');
+    if (!svgObject) return;
+    
+
+    const updateSVGColors = () => {
+        const doc = svgObject.contentDocument;
+
+        if (!doc) return;
+        
+        const svgElement = doc.querySelector('svg');
+        const svgElementPath = svgElement.querySelector('path');
+
+        const styles = getComputedStyle(document.documentElement);
+
+        const color = styles.getPropertyValue('--logo-color').trim();
+        const fill = styles.getPropertyValue('--logo-fill').trim()
+
+        svgElementPath.style.color  = `${color || '#E60062'}`
+        svgElementPath.style.fill  = `${fill || '#E60062'}`
+    };
+
+    svgObject.addEventListener('load', updateSVGColors);
+
+    // Optional: Watch for theme changes
+    const observer = new MutationObserver(updateSVGColors);
+    observer.observe(document.documentElement, {
+        attributes: true,
+        attributeFilter: ['class', 'data-theme']
+    });
+});
